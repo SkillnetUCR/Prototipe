2722b74a18beee30d8607bcd0bc668162e5dd58d
Samueljones1712 <samuelferrari123@outlook.es>
2023-06-05 21:48:47 -0600

    Commit 04/6/2023

--- functions-index.js
+++ functions-index.js
@@ -23,3 +23,16 @@ function showNextItem() {
 
 prevButton.addEventListener('click', showPrevItem);
 nextButton.addEventListener('click', showNextItem);
+
+
+function alertaConstruction() {
+    Swal.fire({
+        title: "No permitido.",
+        html: "La página se encuentra en construcción.",
+        icon: "error",
+        imageWidth: 100,
+        imageHeight: 100,
+        position: "center",
+
+    });
+}
